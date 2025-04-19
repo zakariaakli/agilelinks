88170f6bc67bfc517cbc9c950fc6b8888980a666
Zakaria <zakaria.akli.ensa@gmail.com>
2025-04-19 13:59:05 +0200

    [fix] vercel deploy

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,7 @@ import styles from '../Styles/product.module.css';
 export default function LoginPage() {
   return (
     <>
-        <Seo title="Log in" />
+        <Seo title="Log in" description='description' image='' url=''  />
         <main style={{padding: '20px'}}>
             <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh'}}>
                 <Auth />
