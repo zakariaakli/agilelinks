958a986816cdbe24affb09605226816669ae4c8f
Zakaria <zakaria.akli.ensa@gmail.com>
2025-04-19 13:51:42 +0200

    [fix] vercel deploy

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,26 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    extends: [
-      'next/core-web-vitals', // Includes 'next', 'eslint:recommended', and core web vitals rules
-      'plugin:@typescript-eslint/recommended',
-      'plugin:react/recommended',
-      'plugin:jsx-a11y/recommended',
-      'plugin:prettier/recommended'
-    ],
-    plugins: ['@typescript-eslint', 'react', 'jsx-a11y'],
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,
-      },
-    },
-    rules: {
-      // Customize your own rules here
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      'react/react-in-jsx-scope': 'off', // Next.js doesn't require react to be in scope
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  };
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended', // ✅ this requires the plugin you just installed
+  ],
+  plugins: ['@typescript-eslint', 'react', 'jsx-a11y', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+};
+
--- app/Helpers/dataContext.ts
+++ app/Helpers/dataContext.ts
@@ -1,10 +1,10 @@
 import { createContext, useState } from "react";
-import { ResultData} from "../Models/EnneagramResult";
+import { EnneagramResult} from "../Models/EnneagramResult";
 
-export const DataContext = createContext<ResultData>(
+export const DataContext = createContext<EnneagramResult>(
   { enneagramType1: 0, enneagramType2: 0,
     enneagramType3: 0,enneagramType4:0, enneagramType5:0, enneagramType6:0,
-     enneagramType7:0, enneagramType8:0, enneagramType9:0,  Profession: "",
-   UserId: "" });
+     enneagramType7:0, enneagramType8:0, enneagramType9:0,
+   summary: "" });
 
 
